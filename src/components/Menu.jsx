f14e1249b8ae0862918699b347224f54adeb81fa
Phan Minh Khoi B2014665 <phanthanhbinh089@gmail.com>
2023-07-26 16:22:48 +0700

    Styled Component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    flex: 1
+`
+
+const Menu = () => {
+  return (
+    <Container>
+      MENU
+    </Container>
+  )
+}
+
+export default Menu
